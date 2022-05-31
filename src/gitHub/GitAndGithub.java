a28812476fc5a2c9602ea05b45c8aaaa8d04d0bf
HoshiiKhrystyna <kristinagoshiy@gmail.com>
2022-05-31 18:54:23 -0500

    2nd commit

--- src/gitHub/GitAndGithub.java
+++ src/gitHub/GitAndGithub.java
@@ -4,7 +4,9 @@ public class GitAndGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.print("today we learn github");
+		System.out.print("today is a very good day");
+		
 	}
 
 }
